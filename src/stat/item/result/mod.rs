ce9991d16bb29401b3dc45c5799a461568d22e9c
BlankZhu <BlankZhu47@gmail.com>
2021-12-31 17:03:37 +0800

    update: remove old items & results

--- src/stat/item/result/mod.rs
+++
@@ -1,9 +0,0 @@
-pub mod noop_result;
-pub mod enum_result;
-pub mod num_result;
-pub mod time_result;
-
-pub trait ItemResult {
-    fn get_readable_result(&self) -> String;
-    fn get_json_result(&self) -> String;
-}
